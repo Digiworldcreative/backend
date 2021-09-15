24b700bfffc2e0da6dff16253bd60e3d6f85de6d
guru2108 <guruhariharan07@gmail.com>
2021-09-15 09:57:14 +0530

    second commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c7bf7a73a19f5a8c78e028321fbb52ed'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b5185ad1-0b36-4ddc-8002-f8a601fa3df3'
+};
